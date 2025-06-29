788b6ba4c5777d7609b52aef005abc497999292e
Kartik Tomar <kartiktomar112233@gmail.com>
2025-06-29 23:32:48 +0530

    http init

---
+++ src/singleroute.rs
@@ -0,0 +1,14 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async  fn main() {
+    // build application with single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
